c9eb83287a1c01cf09dd2cc058e9eb96a1e00122
noahjhan <noahjhan11@gmail.com>
2025-03-20 19:14:18 -0500

    if - else - else if branching must be sequential

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,8 +18,7 @@ catch (const std::exception &e)
 }
 */
 
-bool TEST_LEXER()
-{
+bool TEST_LEXER() {
   std::cout << PURPLE << "TEST CASE: Lexer\n" << BAR << RESET << std::endl;
   try {
 
@@ -30,17 +29,19 @@ bool TEST_LEXER()
     }
     // lex.debugPrinter(true /* use true here for verbose printing */);
     std::deque<Token> dq = lex.getTokens();
-  }
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Lexer\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Lexer\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
-  std::cout << GREEN << "TEST CASE PASSED: Lexer\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Lexer\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
-bool TEST_EXPR_TYPES()
-{
+bool TEST_EXPR_TYPES() {
   std::cout << PURPLE << "TEST CASE: Expr Types\n" << BAR << RESET << std::endl;
   try {
     std::string file_name = "data/expression_types.rat";
@@ -73,24 +74,29 @@ bool TEST_EXPR_TYPES()
     assert(lhs_numeric.type == ConstituentToken::TYPE_FLOAT);
 
     assert(std::holds_alternative<Node::NumericLiteral>(*lhs_binary.rhs->expr));
-    auto &lhs_rhs_numeric = std::get<Node::NumericLiteral>(*lhs_binary.rhs->expr);
+    auto &lhs_rhs_numeric =
+        std::get<Node::NumericLiteral>(*lhs_binary.rhs->expr);
     assert(lhs_rhs_numeric.type == ConstituentToken::TYPE_INT);
 
-    assert(std::holds_alternative<Node::NumericLiteral>(*root_binary.rhs->expr));
-    auto &root_binary_rhs_numeric = std::get<Node::NumericLiteral>(*root_binary.rhs->expr);
+    assert(
+        std::holds_alternative<Node::NumericLiteral>(*root_binary.rhs->expr));
+    auto &root_binary_rhs_numeric =
+        std::get<Node::NumericLiteral>(*root_binary.rhs->expr);
     assert(root_binary_rhs_numeric.type == ConstituentToken::TYPE_DOUBLE);
-  }
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Expr Types\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Expr Types\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
-  std::cout << GREEN << "TEST CASE PASSED: Expr Types\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Expr Types\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
 
-bool TEST_EXPR_AST()
-{
+bool TEST_EXPR_AST() {
   std::cout << PURPLE << "TEST CASE: Expr AST\n" << BAR << RESET << std::endl;
   try {
 
@@ -107,8 +113,7 @@ bool TEST_EXPR_AST()
       if (parse.recurseExpr()) {
         throw std::invalid_argument("errpr: multiple expressions");
       }
-    }
-    catch (const std::exception &e) {
+    } catch (const std::exception &e) {
       if (std::string(e.what()) != "token deque empty") {
         throw std::invalid_argument("error: multiple expressions");
       }
@@ -119,34 +124,40 @@ bool TEST_EXPR_AST()
     // auto &root = *nodes.expr;
 
     /// @todo write out full expression
-  }
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Expr AST\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Expr AST\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
-  std::cout << GREEN << "TEST CASE PASSED: Expr AST\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Expr AST\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
 
-bool TEST_VARIABLE_DECLARATION()
-{
-  std::cout << PURPLE << "TEST CASE: Variable Declaration\n" << BAR << RESET << std::endl;
+bool TEST_VARIABLE_DECLARATION() {
+  std::cout << PURPLE << "TEST CASE: Variable Declaration\n"
+            << BAR << RESET << std::endl;
   try {
-  }
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Variable Declaration\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Variable Declaration\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
 
-  std::cout << GREEN << "TEST CASE PASSED: Variable Declaration\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Variable Declaration\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
 
-bool TEST_DISPATCH_SIMPLE()
-{
-  std::cout << PURPLE << "TEST CASE: Dispatch Simple\n" << BAR << RESET << std::endl;
+bool TEST_DISPATCH_SIMPLE() {
+  std::cout << PURPLE << "TEST CASE: Dispatch Simple\n"
+            << BAR << RESET << std::endl;
   try {
     std::string file_name = "data/dispatch_simple.rat";
     auto rat = RatSource(file_name);
@@ -158,20 +169,23 @@ bool TEST_DISPATCH_SIMPLE()
     auto parse = Parser(dq, rat);
     auto ast = parse.dispatch();
     // parse.debugASTPrinter(*ast);
-  }
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Dispatch Simple\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Dispatch Simple\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
 
-  std::cout << GREEN << "TEST CASE PASSED: Dispatch Simple\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Dispatch Simple\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
 
-bool TEST_FUNCTION_CALL()
-{
-  std::cout << PURPLE << "TEST CASE: Function Call\n" << BAR << RESET << std::endl;
+bool TEST_FUNCTION_CALL() {
+  std::cout << PURPLE << "TEST CASE: Function Call\n"
+            << BAR << RESET << std::endl;
   try {
     std::string file_name = "data/function_call.rat";
     auto rat = RatSource(file_name);
@@ -182,20 +196,22 @@ bool TEST_FUNCTION_CALL()
     std::deque<Token> dq = lex.getTokens();
     auto parse = Parser(dq, rat);
     auto ast = parse.dispatch();
-    parse.debugASTPrinter(*ast);
-  }
-  catch (const std::exception &e) {
+    // parse.debugASTPrinter(*ast);
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
-    std::cerr << RED << "TEST CASE FAILED: Function Call\n" << BAR << '\n' << RESET << std::endl;
+    std::cerr << RED << "TEST CASE FAILED: Function Call\n"
+              << BAR << '\n'
+              << RESET << std::endl;
     return false;
   }
 
-  std::cout << GREEN << "TEST CASE PASSED: Function Call\n" << BAR << '\n' << RESET << std::endl;
+  std::cout << GREEN << "TEST CASE PASSED: Function Call\n"
+            << BAR << '\n'
+            << RESET << std::endl;
   return true;
 }
 
-bool TEST_ALL()
-{
+bool TEST_ALL() {
   std::cout << std::endl;
 
   int totalTests = 5; // change per test
@@ -222,9 +238,10 @@ bool TEST_ALL()
 
   if (failedTests > 0) {
     std::cout << RED << "TOTAL TESTS: " << totalTests << std::endl;
-    std::cout << BAR << "\n" << failedTests << " test(s) failed!" << "\n" << BAR << RESET << std::endl;
-  }
-  else {
+    std::cout << BAR << "\n"
+              << failedTests << " test(s) failed!" << "\n"
+              << BAR << RESET << std::endl;
+  } else {
     std::cout << GREEN << "TOTAL TESTS: " << totalTests << std::endl;
     std::cout << BAR << "\nAll tests passed!\n" << BAR << RESET << std::endl;
   }
